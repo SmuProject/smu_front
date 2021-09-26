b82ff375a482f83f09d2645569704d4964f96166
tae2089 <tae2089002@gmail.com>
2021-09-26 19:56:22 +0900

    픽률 소팅 수정

--- src/components/itemcomponents/Itembox.js
+++ src/components/itemcomponents/Itembox.js
@@ -31,12 +31,7 @@ const Itembox = ({ chamiponNumber }) => {
           "https://blog.galbimandudev.com/itemrank/" + chamiponNumber
         );
         console.log(rep.data);
-        const sorting = _.chain(rep.data)
-          .sortBy("pickRate")
-          .reverse()
-          .sortBy("winRate")
-          .reverse()
-          .value();
+        const sorting = _.chain(rep.data).sortBy("pickRate").reverse().value();
         setItemboxList(sorting);
         console.log(sorting);
         setLine("TOP");
